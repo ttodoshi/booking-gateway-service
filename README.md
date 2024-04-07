5f2def6c7c14de2af5e0ba2e5b20bc7a696d689a
todoshi <ttodoshi@users.noreply.github.com>
2024-04-07 12:25:21 +0300

    init: initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Booking
+
+## Gateway Service on Java
+
+Gateway service written on Java with Spring Cloud Gateway
+
+Default port is **8443**
+
+### Build docker container
+
+```shell
+docker build . -t ghcr.io/ttodoshi/booking-gateway-service:latest
+```
---
+++ src/test/java/org/booking/gateway/BookingGatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.booking.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingGatewayServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
